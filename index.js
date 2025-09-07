1f703b8af3bb3041ff2d2045eec0ba2784b9ab22
Surveerssg <btech.surveer@gmail.com>
2025-09-07 18:23:38 +0530

    Psychiatrist auth added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const cors =  require('cors');
 const dotenv =  require('dotenv');
 const chatRoutes =  require('./routes/chatRoutes.js');
+const psychiatristRoutes = require('./routes/psychiatristRoutes.js');
 // const authRoutes =  require('./routes/authRoutes.js');
 const dbRoutes =  require('./routes/dbRoutes.js');
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/api', chatRoutes);
 // app.use('/api', authRoutes);
 app.use('/api', dbRoutes);
+app.use('/api', psychiatristRoutes);
 
 app.listen(4000, () => {
   console.log('Backend running on http://localhost:4000');
