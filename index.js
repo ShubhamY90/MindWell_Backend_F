e16ee4e0fbf7f9f666ddb2b1de67ef1d7a50f2ba
Abhishek <abhishekroy2342005@gmail.com>
2025-09-14 16:39:51 +0530

    Everything Okay till This point and deployed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://rnchatbot.netlify.app'],
+  origin: ['*'],
   credentials: true,
 }));
 app.use(express.json());
